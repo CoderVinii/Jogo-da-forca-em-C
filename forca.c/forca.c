972a481a0667858a590c196fb4085a59f6678e87
CoderVinii <viniciusdmfernandes@gmail.com>
2024-11-01 00:12:31 -0300

    Atualização

--- forca.c/forca.c
+++ forca.c/forca.c
@@ -45,61 +45,76 @@ void desenhaforca()
     printf("\n");
 }
 
-void adicionapalavra() {
+void adicionapalavra()
+{
 
     char quer;
-     
-     printf("Voce deseja adicionar uma nova palavra no jogo? (S/N)");
-     scanf(" %c", &quer);
-     
-    if(quer == 'S') {
+
+    printf("Voce deseja adicionar uma nova palavra no jogo? (S/N)");
+    scanf(" %c", &quer);
+
+    if (quer == 'S')
+    {
 
         char novapalavra[20];
         printf("Qual a nova palavra? ");
         scanf("%s", novapalavra);
 
-        FILE* f;
+        FILE *f;
 
         f = fopen("palavras.txt", "r+");
+        if (f == 0)
+        {
+            printf("Desculpe, o banco de dados nao esta disponivel\n\n");
+            exit(1);
+        }
 
         int qtd;
         fscanf(f, "%d", &qtd);
         qtd++;
 
+        fseek(f, 0, SEEK_SET);
+        fprintf(f, "%d", qtd);
+
+        fseek(f, 0, SEEK_END);
         fprintf(f, "\n%s", novapalavra);
 
         fclose(f);
     }
-     
 }
 
 void escolhepalavra()
 {
-    FILE* f;
+    FILE *f;
 
-   f = fopen("palavras.txt", "r");
-   if(f == 0) {
-    printf("Desculpe, o banco de dados nao esta disponivel\n\n");
-    exit(1);
-   }
+    f = fopen("palavras.txt", "r");
+    if (f == 0)
+    {
+        printf("Desculpe, o banco de dados nao esta disponivel\n\n");
+        exit(1);
+    }
 
-   int qtddepalavras;
-   fscanf(f, "%d", &qtddepalavras);
-   
-   srand(time(0));
-   int randomico = rand() % qtddepalavras;
+    int qtddepalavras;
+    fscanf(f, "%d", &qtddepalavras);
 
-   for(int i = 0; i <= randomico; i++); {
-    fscanf(f, "%s", palavrasecreta);
-   }
+    srand(time(0));
+    int randomico = rand() % qtddepalavras;
 
+    for (int i = 0; i <= randomico; i++)
+        ;
+    {
+        fscanf(f, "%s", palavrasecreta);
+    }
 
-   fclose(f);
+    fclose(f);
 }
 
-int acertou() {
-    for(int i = 0; i < strlen(palavrasecreta); i++) {
-        if(!jachutou(palavrasecreta[i])) {
+int acertou()
+{
+    for (int i = 0; i < strlen(palavrasecreta); i++)
+    {
+        if (!jachutou(palavrasecreta[i]))
+        {
             return 0;
         }
     }
